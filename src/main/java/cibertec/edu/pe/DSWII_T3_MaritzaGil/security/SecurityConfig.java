70aff5be9cb26b68899c95e7a98425ac3f86e292
AnaGil27 <gilyalta27@gmail.com>
2023-11-25 22:43:42 -0500

    Cuarto Commit

--- src/main/java/cibertec/edu/pe/DSWII_T3_MaritzaGil/security/SecurityConfig.java
+++ src/main/java/cibertec/edu/pe/DSWII_T3_MaritzaGil/security/SecurityConfig.java
@@ -12,12 +12,19 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @AllArgsConstructor
 @EnableMethodSecurity
@@ -46,7 +53,13 @@ public class SecurityConfig{
     @Bean
     public UserDetailsService userDetailsService() {
         return username -> {
-            if (username.equals("supervisor")) {
+            if (username.equals("admin")) {
+                return User.builder()
+                        .username("admin")
+                        .password(passwordEncoder().encode("password"))
+                        .roles("Administrador")
+                        .build();
+            } else if (username.equals("supervisor")) {
                 return User.builder()
                         .username("supervisor")
                         .password(passwordEncoder().encode("password"))
@@ -56,7 +69,19 @@ public class SecurityConfig{
             throw new UsernameNotFoundException("Usuario no encontrado");
         };
     }
-
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/filesdoc").hasRole("Administrador")
+                .antMatchers("/filespdf").hasRole("Supervisor")
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
 
 
     @Bean
@@ -74,5 +99,18 @@ public class SecurityConfig{
     public AuthenticationManager authenticationManager(
             AuthenticationConfiguration configuration) throws  Exception{
         return configuration.getAuthenticationManager();
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://www.cibertec.edu.pe"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
