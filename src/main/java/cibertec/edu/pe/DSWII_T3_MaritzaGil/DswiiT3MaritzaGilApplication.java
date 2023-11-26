57a8593592cc9579cde3b99a3bcbdd91c476de13
AnaGil27 <gilyalta27@gmail.com>
2023-11-25 21:26:27 -0500

    Primer Commit

---
+++ src/main/java/cibertec/edu/pe/DSWII_T3_MaritzaGil/DswiiT3MaritzaGilApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DSWII_T3_MaritzaGil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiT3MaritzaGilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiT3MaritzaGilApplication.class, args);
+	}
+
+}
